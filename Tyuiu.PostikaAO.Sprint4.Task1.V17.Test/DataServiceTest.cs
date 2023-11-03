3b2263f233c963022d86ff72a8fb05e7cfb42318
PostikaArtem <Tyuiu.PostikaAO@outlook.com>
2023-11-03 21:49:54 +0500

    Task1

---
+++ Tyuiu.PostikaAO.Sprint4.Task1.V17.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PostikaAO.Sprint4.Task1.V17.Lib;
+
+namespace Tyuiu.PostikaAO.Sprint4.Task1.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 1, 7, 6, 3, 2, 5, 7, 6, 7, 6, 5, 4, 7, 6 };
+            int res = ds.Calculate(numsArray);
+            int a = 30;
+            Assert.AreEqual(30, res);
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.PostikaAO.Sprint4.Task1.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PostikaAO.Sprint4.Task1.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PostikaAO.Sprint4.Task1.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4485dbcd-6d12-45d7-80e6-9067d5a6c493")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
