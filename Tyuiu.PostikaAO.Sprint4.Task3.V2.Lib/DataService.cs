84254f861334bfeb1923d5ea019cb2adca5cb717
PostikaArtem <Tyuiu.PostikaAO@outlook.com>
2023-11-05 23:54:55 +0500

    Task3

---
+++ Tyuiu.PostikaAO.Sprint4.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.PostikaAO.Sprint4.Task3.V2.Lib
+{
+    public class DataService : ISprint4Task3V2
+    {
+        public int Calculate(int[,] array)
+        {
+            int maxValue=0;
+
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                if (maxValue < array[0, i])
+                {
+                    maxValue = array[0, i];
+                }
+            }
+            return maxValue;
+        }
+    }
+}
---
+++ Tyuiu.PostikaAO.Sprint4.Task3.V2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PostikaAO.Sprint4.Task3.V2.Lib;
+
+namespace Tyuiu.PostikaAO.Sprint4.Task3.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Постика А. О. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант 2                                                               *");
+            Console.WriteLine("* Выполнил: Постика Артём Олегович | ИИПб-23-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 2 до 8. Найдите максимальный     *");
+            Console.WriteLine("* элемент в первой строке массива. { 4, 3, 2, 8, 7 }, { 4, 6, 5, 5, 7 },  *");
+            Console.WriteLine("* { 3, 8, 8, 3, 5 }, { 5, 7, 5, 4, 4 }, { 8, 8, 3, 7, 6 }                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив: ");
+            int[,] mas2 = new int[5, 5] { { 4, 3, 2, 8, 7 }, { 4, 6, 5, 5, 7 }, { 3, 8, 8, 3, 5 }, { 5, 7, 5, 4, 4 }, { 8, 8, 3, 7, 6 } };
+            int rows = mas2.GetUpperBound(0) + 1;
+            int columns = mas2.Length / rows;
+            for(int i=0; i<rows; i++)
+            {
+                for(int j=0; j < columns; j++)
+                {
+                    Console.Write($"{mas2[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mas2);
+            Console.WriteLine("Максимальный элемент в первой строке массива = " + res);
+            Console.ReadKey();
+        }
+    }
+}
