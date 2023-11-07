9c104f62375c4ea7d42eb7efb1a75c4202c6fca7
PostikaArtem <Tyuiu.PostikaAO@outlook.com>
2023-11-08 00:29:43 +0500

    Task4

---
+++ Tyuiu.PostikaAO.Sprint4.Task4.V29.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.PostikaAO.Sprint4.Task4.V29.Lib
+{
+    public class DataService : ISprint4Task4V29
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            int c=0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] % 2 == 0)
+                    {
+                        c += matrix[i, j];
+                    }
+                }
+            }
+            return c;
+        }
+    }
+}
---
+++ Tyuiu.PostikaAO.Sprint4.Task4.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PostikaAO.Sprint4.Task4.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PostikaAO.Sprint4.Task4.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("550479a2-7bb4-4a02-ad31-8cf7dc6edcb0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
